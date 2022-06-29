08bee5f467dafc2982fd5b8086fdd5e58e3af7b4
sinamokhtar <sinamokhtar@tamu.edu>
2022-06-29 16:43:09 -0500

    initial commit

--- Functions/community_risk_level_state.R
+++ Functions/community_risk_level_state.R
@@ -8,17 +8,24 @@ library(tigris)
 library(sf)
 
 census_api_key("7e83aa1d195fd7cd921e4ac747998c618f05460d")
-stateGeo =  get_acs(geography = "state",
-                         variables = "B01001_001",
-                         year = 2020)%>%
-    shift_geometry() %>%
-    rename("state" = NAME)
 
-v17 <- load_variables(year = 2020, dataset = "acs5")
+
 # load datasets
 load("Result/hospital_utilization_county.RDA")
 load("Result/CDC_community_transmission_county_historical.RDA")
 load("Result/CDC_community_level_county.RDA")
+
+
+
+# get state population
+statePop =  get_acs(geography = "state",
+                    variables = "B01001_001",
+                    year = 2020,
+                    geometry = FALSE) %>%
+    rename("state" = NAME,
+           "population" = estimate) %>%
+    select(state, population)
+
 
 # getting "new case" from 
 # "CDC United States COVID-19 County Level of Community Transmission Historical Changes" dataset
@@ -47,105 +54,92 @@ hospital_state = hospital_utilization %>%
 
 
 
-# merge "newcase" and "Hospital Utilization" datasets
-merged_newcase = merge(hospital_state,
+# merge all datasets 
+merged_data = merge(hospital_state,
                        new_cases,
-                       by=c("date", "state"))
+                       by=c("date", "state")) %>%
+    inner_join(statePop, by = "state")
 
 
-# add state population from
-# "United States COVID-19 Community Levels by County"
-county_pop = CDC_community_level_county %>%
-    dplyr::filter(date_updated == "2022-03-24") %>%
-    dplyr::select(county_fips, 
-                  population) %>%
-    rename(fips_code = county_fips) %>%
-    mutate(fips_code = as.numeric(fips_code))
 
-# add counties population to dataset
-merged_data = merge(merged_newcase,
-                    county_pop,
-                    by="fips_code")
-
-# compute hospital_admission_per100
-community_level_county = merged_data %>%
-    mutate(hospital_admission_per100 = round((hospital_admissions/population)*100000))
-
-# remove NA value
-community_level_county = community_level_county %>%
+# compute hospital_admission_per100 and remove NA
+community_level_state = merged_data %>%
+    mutate(hospital_admission_per100 = round((hospital_admissions_state/population)*100000)) %>%
     drop_na(new_case,
             hospital_admission_per100,
             bed_utilization)
 
 
+
+
 # compute community_level
 low_index = 
-    (community_level_county$new_case < 200 & 
-         community_level_county$hospital_admission_per100 < 10) | 
-    (community_level_county$new_case < 200 &
-         community_level_county$bed_utilization < 10)
+    (community_level_state$new_case < 200 & 
+         community_level_state$hospital_admission_per100 < 10) | 
+    (community_level_state$new_case < 200 &
+         community_level_state$bed_utilization < 10)
 
 medium_index = 
-    (community_level_county$new_case < 200 &
-         (community_level_county$hospital_admission_per100 >= 10 &
-              community_level_county$hospital_admission_per100 < 20)) |
-    (community_level_county$new_case < 200 &
-         (community_level_county$bed_utilization >= 10 &
-              community_level_county$bed_utilization < 15)) |
-    (community_level_county$new_case >= 200 &
-         community_level_county$hospital_admission_per100 < 10) |
-    (community_level_county$new_case >= 200 &
-         community_level_county$bed_utilization < 10)
+    (community_level_state$new_case < 200 &
+         (community_level_state$hospital_admission_per100 >= 10 &
+              community_level_state$hospital_admission_per100 < 20)) |
+    (community_level_state$new_case < 200 &
+         (community_level_state$bed_utilization >= 10 &
+              community_level_state$bed_utilization < 15)) |
+    (community_level_state$new_case >= 200 &
+         community_level_state$hospital_admission_per100 < 10) |
+    (community_level_state$new_case >= 200 &
+         community_level_state$bed_utilization < 10)
 
 high_index = 
-    (community_level_county$new_case < 200 & 
-         community_level_county$hospital_admission_per100 >= 20) | 
-    (community_level_county$new_case < 200 & 
-         community_level_county$bed_utilization >= 15) |
-    (community_level_county$new_case >= 200 & 
-         community_level_county$hospital_admission_per100 >= 10) | 
-    (community_level_county$new_case >= 200 & 
-         community_level_county$bed_utilization >= 10) 
+    (community_level_state$new_case < 200 & 
+         community_level_state$hospital_admission_per100 >= 20) | 
+    (community_level_state$new_case < 200 & 
+         community_level_state$bed_utilization >= 15) |
+    (community_level_state$new_case >= 200 & 
+         community_level_state$hospital_admission_per100 >= 10) | 
+    (community_level_state$new_case >= 200 & 
+         community_level_state$bed_utilization >= 10) 
 
 #########################LOW MEDIUM HIGH #################################
-community_level_county$community_level = NA
-community_level_county$community_level[low_index] = "Low"
-community_level_county$community_level[medium_index] = "Medium"
-community_level_county$community_level[high_index] = "High"
+community_level_state$community_level = NA
+community_level_state$community_level[low_index] = "Low"
+community_level_state$community_level[medium_index] = "Medium"
+community_level_state$community_level[high_index] = "High"
 
-community_level_LMH = community_level_county %>%
+community_level_LMH_state = community_level_state %>%
     drop_na(community_level)
 
-save(community_level_LMH,
-     file="Result/CDC_community_level_county_computed_low_medium_high.RDA")
+save(community_level_LMH_state,
+     file="Result/CDC_community_level_state_computed_low_medium_high.RDA")
 
 
 ####################### HIGH amd MEDIUM MERGED ###################
-community_level_county$community_level = NA
-community_level_county$community_level[low_index] = "Low"
-community_level_county$community_level[medium_index] = "High + Medium"
-community_level_county$community_level[high_index] = "High + Medium"
+community_level_state$community_level = NA
+community_level_state$community_level[low_index] = "Low"
+community_level_state$community_level[medium_index] = "High + Medium"
+community_level_state$community_level[high_index] = "High + Medium"
 
 #remove NA value
-community_level_MH = community_level_county %>%
+community_level_MH_state = community_level_state %>%
     drop_na(community_level)
 
 #save the file
-save(community_level_MH,
-     file="Result/CDC_community_level_county_computed_merged_high_medium.RDA")
+save(community_level_MH_state,
+     file="Result/CDC_community_level_state_computed_merged_high_medium.RDA")
 
 ###################### LOW and MEDIUM MERGED ###################
-community_level_county$community_level = NA
-community_level_county$community_level[low_index] = "Low + Medium"
-community_level_county$community_level[medium_index] = "Low + Medium"
-community_level_county$community_level[high_index] = "High"
+community_level_state$community_level = NA
+community_level_state$community_level[low_index] = "Low + Medium"
+community_level_state$community_level[medium_index] = "Low + Medium"
+community_level_state$community_level[high_index] = "High"
 
 
 
-community_level_LM = community_level_county %>%
+community_level_LM_state = community_level_state %>%
     drop_na(community_level)
 
-save(community_level_LM,
-     file="Result/CDC_community_level_county_computed_merged_low_medium.RDA")
+save(community_level_LM_state,
+     file="Result/CDC_community_level_state_computed_merged_low_medium.RDA")
 
 
